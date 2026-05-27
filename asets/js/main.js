d22530d9c93eca9fc031e405fae9a88d39ea7fd0
Yusqi_0011 <firdansardadan@gmail.com>
2026-05-28 02:26:59 +0700

    Add files via upload

---
+++ asets/js/main.js
@@ -0,0 +1,48 @@
+/*================ mengatur Navbar =======================*/
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+
+/*================ Scroll section active link =======================*/
+let sections = document.querySelectorAll('section');
+let navlink = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+
+        if(top >= offset && top <offset + height){
+            navLinks.forEach.apply(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    /*================= Sticky navbar ================================*/
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', windows.scrollY > 100);
+
+    /*================= remove icon dan navbar ================================*/
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+
+/*================ Scroll reveal  =======================*/
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portofolio-box, .contact form', {origin: 'buttom'});
+ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
